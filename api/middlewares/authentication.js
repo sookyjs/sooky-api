dd9edfdffeccc50ea825d5bfcc0206163df7667a
Salim Benfarhat <hello@salim.link>
2024-11-05 18:18:07 +0100

    fix: redefine and export authentication middleware correctly in authentication.js

--- api/middlewares/authentication.js
+++ api/middlewares/authentication.js
@@ -1,17 +1,20 @@
 import jwt from 'jsonwebtoken';
 import config from '../../config/index.js';
 
-export default (req, res, next) => {
-  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+const authentication = (req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
-        const token = req.headers.authorization.split(' ')[1]
-        const decoded = jwt.verify(token, config.jwtSecret);
-        //attach the user to the jobs route
-        req.user = {_id : decoded.id};
-        next();
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, config.jwtSecret);
+      req.user = { _id: decoded.id };
+      next();
     } catch (error) {
-        res.status(401).json({success : false, error : error.message});
-        throw new Error('Not Authorized')
+      res.status(401).json({ success: false, error: error.message });
+      throw new Error('Not Authorized');
     }
+  } else {
+    res.status(401).json({ message: 'No token, authorization denied' });
   }
-}
+};
+
+export default authentication;
